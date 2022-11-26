667c9443b7584d71905d42e4a2e6bc4123e6d3c7
kxh001 <728096148@qq.com>
2022-11-26 03:04:09 -0800

    adjust project structure

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="diffusion",
-    py_modules=["utilsiddpm"],
+    py_modules=["utilsitd"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
